bf0751370336753510e7fbf45bc75adacdbb140c
srubel1 <srubel.cse.bd@gmail.com>
2023-02-07 00:17:12 +0600

    done

---
+++ OOP-Lab-Task-1/Practice.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.mavenproject1;
+
+
+public class Practice {
+    public static void main(String[] args) {
+       String Name =("Rubel Mian");
+       System.out.println(Name);
+    }
+}
